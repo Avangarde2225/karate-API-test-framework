a33ae98ebd27a02c6ccac5123bb37ab96ea9b9c5
Serdar <serdar2225@gmail.com>
2020-09-07 19:20:34 -0400

    cucumber html reporting

--- src/test/java/feature/apiTestRunner.java
+++ src/test/java/feature/apiTestRunner.java
@@ -1,9 +1,38 @@
 package feature;
 
-import com.intuit.karate.junit4.Karate;
-import org.junit.runner.RunWith;
+import com.intuit.karate.Results;
+import com.intuit.karate.Runner;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import net.masterthought.cucumber.Configuration;
+import net.masterthought.cucumber.ReportBuilder;
+import org.apache.commons.io.FileUtils;
+import static org.junit.Assert.*;
+import org.junit.BeforeClass;
+import org.junit.Test;
 
-@RunWith(Karate.class)
+import static org.junit.Assert.assertTrue;
+
+//@RunWith(Karate.class)
 public class apiTestRunner {
+
+    @Test
+    public void testParallel() {
+        System.setProperty("karate.env", "demo"); // ensure reset if other tests (e.g. mock) had set env in CI
+        Results results = Runner.path("classpath:demo").tags("~@ignore").parallel(5);
+        generateReport(results.getReportDir());
+        assertTrue(results.getErrorMessages(), results.getFailCount() == 0);
+    }
+
+    public static void generateReport(String karateOutputPath) {
+        Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
+        List<String> jsonPaths = new ArrayList(jsonFiles.size());
+        jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
+        Configuration config = new Configuration(new File("target"), "demo");
+        ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
+        reportBuilder.generateReports();
+    }
 
 }
